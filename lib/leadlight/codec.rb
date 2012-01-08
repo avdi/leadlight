22c0cdda6e7371478b0a0fc912386f8d5c5e42e1
Avdi Grimm <avdi@avdi.org>
2012-01-08 03:27:54 -0500

    Handle encoding

--- lib/leadlight/codec.rb
+++ lib/leadlight/codec.rb
@@ -1,12 +1,61 @@
 module Leadlight
   class Codec
+    Strategy ||= Struct.new(:name, :encoder, :decoder, :patterns)
+
+    def self.strategies
+      @strategies ||= []
+    end
+
+    def self.strategy(name, encoder, decoder, patterns)
+      strategies << Strategy.new(name, encoder, decoder, Array(patterns))
+    end
+
+    strategies.clear
+
+    strategy :text,             
+             ->(rep, options)         {rep.to_s},
+             ->(entity_body, options) {entity_body},
+             %r{^text/plain}
+
+    strategy :json, 
+             MultiJson.method(:encode),
+             MultiJson.method(:decode), 
+             [%r{^application/json}, %r{\+json$}]
+
     def decode(content_type, entity_body, options={})
-      case content_type.to_s.strip.split.first
-      when %r{^text/plain}
-        entity_body
-      when %r{^application/json}, %r{\+json$}
-        ::MultiJson.decode(entity_body, options)
-      else
+      transcode(:decode, content_type, entity_body, options)
+    end
+
+    def encode(content_type, representation, options={})
+      transcode(:encode, content_type, representation, options)
+    end
+
+    private
+
+    def transcode(direction, content_type, input, options)
+      fallback = unknown_type_handler(content_type)
+      strategy = fetch_strategy(content_type, &fallback)
+      transcoder = case direction
+                   when :encode then strategy.encoder
+                   when :decode then strategy.decoder
+                   else raise ArgumentError, "Should never get here"
+                   end
+      transcoder.(input, options)
+    end
+
+    def strategies
+      self.class.strategies
+    end
+
+    def fetch_strategy(content_type, &fallback)
+      content_type = content_type.to_s.strip.split.first
+      strategies.detect(fallback) { |strategy|
+        strategy.patterns.any?{|pattern| pattern === content_type}
+      }
+    end
+
+    def unknown_type_handler(content_type)
+      -> do
         raise ArgumentError, "Unrecognized content type #{content_type.inspect}"
       end
     end
--- spec/leadlight/codec_spec.rb
+++ spec/leadlight/codec_spec.rb
@@ -3,10 +3,16 @@ require 'leadlight/codec'
 
 module Leadlight
   describe Codec do
-    let(:representation) { stub(:representation) }
-    let(:entity_body)    { stub(:entity_body)    }
+    let(:subject)        {
+      # Due to the static init in Codec, we need to reload the class
+      # after stubs/mocks are set up
+      load 'leadlight/codec.rb'
+      Codec.new
+    }
+    let(:representation) { stub(:representation)  }
+    let(:entity_body)    { stub(:entity_body)  }
 
-    it 'decodes JSON with multi_json' do
+    it 'decodes JSON' do
       MultiJson.should_receive(:decode).
         with(entity_body, anything).
         and_return(representation)
@@ -43,6 +49,45 @@ module Leadlight
       MultiJson.should_receive(:decode).
         with(anything, {foo: 42})
       subject.decode('application/json', entity_body, foo: 42)
+    end
+
+    it 'decodes JSON with multi_json' do
+      MultiJson.should_receive(:decode).
+        with(entity_body, anything).
+        and_return(representation)
+      subject.decode('application/json', entity_body).
+        should equal(representation)
+    end
+
+    it 'encodes types ending in +json as JSON' do
+      MultiJson.should_receive(:encode).
+        with(representation, anything).
+        and_return(entity_body)
+      subject.encode('application/vnc.example.user+json foo=bar', representation).
+        should equal(entity_body)
+    end
+
+    it 'encodes text/plain as a string' do
+      subject.encode('text/plain', representation).should eq(representation.to_s)
+    end
+
+    it 'barfs on encoding unrecognized content-type' do
+      expect{ subject.encode('xyzzy', representation) }.
+        to raise_error(ArgumentError)
+    end
+
+    it 'ignores junk after the content type for encoding' do
+      subject.encode('text/plain foo=bar', representation).should eq(representation.to_s)
+    end
+
+    it 'ignores whitespace in the content type for encoding' do
+      subject.encode('  text/plain foo=bar', representation).should eq(representation.to_s)
+    end
+
+    it 'passes options to the JSON encoder' do
+      MultiJson.should_receive(:encode).
+        with(anything, {foo: 42})
+      subject.encode('application/json', representation, foo: 42)
     end
   end
 end
