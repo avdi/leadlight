f8ede8bc9f31dbf69f6365cff1eaac6d5a112849
Avdi Grimm <avdi@avdi.org>
2012-01-15 16:23:07 -0500

    Remove TODO

--- lib/leadlight/service_middleware.rb
+++ lib/leadlight/service_middleware.rb
@@ -12,13 +12,6 @@ module Leadlight
       env[:leadlight_service] = @service
       env[:request_headers]['Accept'] = default_accept_types
       @app.call(env).on_complete do |env|
-        # TODO get a callback in here for stuff that needs to happen 
-        # BEFORE the representation, such as error code checking
-        #
-        # Actually, this should construct an Error representation if
-        # non-200, and then raise_on_error should raise it.
-        #
-        # And tints shouldn't tint error representations.
         env[:leadlight_representation] = represent(env)
       end
     end
